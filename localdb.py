23d667c9c2c2977df400e3f155be613e3fa010d0
a16783869 <Khudyakov.K.Ko@sberbank.ru>
2021-04-06 17:11:00 +0300

    тест сборки образа

--- localdb.py
+++ localdb.py
@@ -1,4 +1,7 @@
 
 uid_to_table_mapping: dict = {
-    "7dbbccad-a746-4f3d-ac3a-e22327e1bcf9": "vectors"
+    "7dbbccad-a746-4f3d-ac3a-e22327e1bcf9": "vectors",
+    "6dbbccad-a746-4f3d-ac3a-e22327e1ybg8": "vectors_from_vkclube",
+    "6dbbccad-a746-4f3d-ac3a-e22327e1ybg9": "vectors_high_resolution_low_jitter",
+    "lolo": "lolo"
 }
